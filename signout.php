cfdbffcd5e2943ed6df45dd9dc5ed5a4e15b63e8
robertpelloni <pelloni.robert@gmail.com>
2025-09-11 13:25:18 -0400

    "Feat: Complete security overhaul and modernization"

--- signout.php
+++ signout.php
@@ -18,16 +18,36 @@
     along with FWBer.  If not, see <https://www.gnu.org/licenses/>.
 */
 
-	session_start();
-	
-	require_once("_names.php");
-	
-	//delete cookies
-	setcookie("email","",time()-1000,'/',".".getSiteDomain());
-	setcookie("token","",time()-1000,'/',".".getSiteDomain());
-	
-	session_destroy();
+require_once('_init.php');
+session_start();
 
-	header('Location: '.getSiteURL());
-    exit();
+// Invalidate the session token in the database
+$token = $_SESSION['token'] ?? $_COOKIE['token'] ?? null;
+if ($token) {
+    $securityManager->invalidateSession($token);
+}
+
+// Unset all of the session variables.
+$_SESSION = array();
+
+// If it's desired to kill the session, also delete the session cookie.
+// Note: This will destroy the session, and not just the session data!
+if (ini_get("session.use_cookies")) {
+    $params = session_get_cookie_params();
+    setcookie(session_name(), '', time() - 42000,
+        $params["path"], $params["domain"],
+        $params["secure"], $params["httponly"]
+    );
+}
+
+// Finally, destroy the session.
+session_destroy();
+
+// Delete the long-lived cookies
+setcookie("email", "", time() - 3600, '/');
+setcookie("token", "", time() - 3600, '/');
+
+// Redirect to homepage
+header('Location: /?message=logged_out');
+exit();
 ?>
