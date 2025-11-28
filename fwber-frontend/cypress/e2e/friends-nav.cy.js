cb47580350d27ceaf2defe03b4762ef671ba7579
Robert Pelloni <pelloni.robert@gmail.com>
2025-11-28 13:43:36 -0500

    Fix: Stop tracking sub-repo files in root repository

--- fwber-frontend/cypress/e2e/friends-nav.cy.js
+++
@@ -1,8 +0,0 @@
-describe('Navigation', () => {
-  it('should navigate to the friends page', () => {
-    cy.visit('/matches');
-    cy.get('a[href*="friends"]').click();
-    cy.url().should('include', '/friends');
-    cy.get('h1').should('contain', 'Friends');
-  });
-});
