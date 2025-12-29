58b7db2adc6e47478821c4af3460fb12b5f60a21
Robert Pelloni <pelloni.robert@gmail.com>
2025-12-29 06:35:43 -0500

    security: enable sentry integration for exception handling

--- fwber-backend/bootstrap/app.php
+++ fwber-backend/bootstrap/app.php
@@ -27,5 +27,5 @@ return Application::configure(basePath: dirname(__DIR__))
         ]);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
-        //
+        \Sentry\Laravel\Integration::handles($exceptions);
     })->create();
